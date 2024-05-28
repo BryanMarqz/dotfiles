5b27215023e2dcc1881e059128bc335a37c9c0b6
Bryan Márquez <69647237+BryanMarqz@users.noreply.github.com>
2024-05-28 14:26:03 -0400

    Fixing preferences so it does not save credentials.

--- macOS/mac_bootstrap_scripts/preferences/export_preferences.sh
+++ macOS/mac_bootstrap_scripts/preferences/export_preferences.sh
@@ -2,8 +2,20 @@
 
 export_dir=~/.dotfiles/macOS/mac_bootstrap_scripts/preferences/plist
 
-# Export all preferences
-for domain in $(defaults domains | tr ',' '
-'); do
+# List of domains to exclude
+exclude_domains=(
+    "com.apple.KeychainAccess"
+    "com.apple.security.keychain"
+    "com.apple.internetaccounts"
+    "com.apple.accountsd"
+)
+
+# Convert the array to a pattern string
+exclude_pattern=$(printf "|%s" "${exclude_domains[@]}")
+exclude_pattern=${exclude_pattern:1}
+
+# Export all preferences except excluded ones
+for domain in $(defaults domains | tr ',' '\n' | grep -Ev "$exclude_pattern"); do
+    mkdir -p $export_dir
     defaults export $domain $export_dir/$domain.plist
 done
