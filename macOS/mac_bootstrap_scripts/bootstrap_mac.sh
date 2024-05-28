eee578f430f8829cc438bf90c71527563f08affd
Bryan Marquez <69647237+BryanMarqz@users.noreply.github.com>
2024-05-28 00:16:17 -0400

    Update bootstrap_mac.sh

--- macOS/mac_bootstrap_scripts/bootstrap_mac.sh
+++ macOS/mac_bootstrap_scripts/bootstrap_mac.sh
@@ -5,17 +5,14 @@
 # Step 1: Install Command Line Tools
 ./scripts/install_command_line_tools.sh
 
-# Step 2: Clone Repo (use "ssh" as argument to clone using SSH, otherwise will use HTTPS)
-./scripts/clone_repo.sh ssh
-
-# Step 3: Create Symlinks
+# Step 2: Create Symlinks
 ./scripts/create_symlinks.sh
 
-# Step 4: Import Preferences
+# Step 3: Import Preferences
 ./../preferences/import_preferences.sh
 
-# Step 5: Install Homebrew and Packages
+# Step 4: Install Homebrew and Packages
 ./scripts/install_homebrew_and_packages.sh
 
-# Step 6: Add cron job
+# Step 5: Add cron job
 ./scripts/add_cron_job.sh
