c471ccc7601b4ec25c3581ef283c1d920be05d6d
Bryan Marquez <69647237+BryanMarqz@users.noreply.github.com>
2024-05-28 00:23:11 -0400

    Update bootstrap_mac.sh

--- macOS/mac_bootstrap_scripts/bootstrap_mac.sh
+++ macOS/mac_bootstrap_scripts/bootstrap_mac.sh
@@ -2,17 +2,14 @@
 
 # Run all steps to bootstrap a new Mac
 
-# Step 1: Install Command Line Tools
-./scripts/install_command_line_tools.sh
-
-# Step 2: Create Symlinks
+# Step 1: Create Symlinks
 ./scripts/create_symlinks.sh
 
-# Step 3: Import Preferences
+# Step 2: Import Preferences
 ./../preferences/import_preferences.sh
 
-# Step 4: Install Homebrew and Packages
+# Step 3: Install Homebrew and Packages
 ./scripts/install_homebrew_and_packages.sh
 
-# Step 5: Add cron job
+# Step 4: Add cron job
 ./scripts/add_cron_job.sh
