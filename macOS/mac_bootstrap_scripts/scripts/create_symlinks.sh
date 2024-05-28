5610fa2197e8a1c28b6d348519e963d79a6db4bb
Bryan Marquez <69647237+BryanMarqz@users.noreply.github.com>
2024-05-28 00:13:28 -0400

    Update create_symlinks.sh

--- macOS/mac_bootstrap_scripts/scripts/create_symlinks.sh
+++ macOS/mac_bootstrap_scripts/scripts/create_symlinks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Create symlinks in the Home directory to the real files in the repo
-ln -s ~/.dotfiles/macOS/.ssh ~/.ssh
 ln -s ~/.dotfiles/macOS/.CFUserTextEncoding ~/.CFUserTextEncoding
 ln -s ~/.dotfiles/macOS/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/macOS/.gitignore_global ~/.gitignore_global
