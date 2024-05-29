a8296e3315cfe1d833d0d4debcdd20b5978d58b6
Bryan Marquez <69647237+BryanMarqz@users.noreply.github.com>
2024-05-29 01:40:44 -0400

    Update export_preferences.sh

--- macOS/mac_bootstrap_scripts/preferences/export_preferences.sh
+++ macOS/mac_bootstrap_scripts/preferences/export_preferences.sh
@@ -8,7 +8,23 @@ exclude_domains=(
     "com.apple.security.keychain"
     "com.apple.internetaccounts"
     "com.apple.accountsd"
-    "com.apple.FaceTime"
+    "com.apple.iCloudHelper"
+    "com.apple.ids.service"
+    "com.apple.imagent"
+    "com.apple.imservice"
+    "com.apple.security"
+    "com.apple.cloudd"
+    "com.apple.CloudDocs"
+    "com.apple.preferences.icloud"
+    "com.apple.familycircled"
+    "com.apple.Family"
+    "com.apple.appleaccount"
+    "com.apple.findmy"
+    "com.apple.coreservices.appleidauthentication"
+    "com.apple.coreservices.cloudkeychainproxy"
+    "com.apple.icloud.fmip"
+    "com.apple.icloud.findmydevicelock"
+    "com.apple.icloud.searchpartyuser"
 )
 
 # Convert the array to a pattern string
@@ -20,3 +36,4 @@ for domain in $(defaults domains | tr ',' '\n' | grep -Ev "$exclude_pattern"); d
     mkdir -p $export_dir
     defaults export $domain $export_dir/$domain.plist
 done
+
